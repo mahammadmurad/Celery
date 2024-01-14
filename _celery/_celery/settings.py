d718b6006a5fb70ca41b09d783423afc984ca087
mahammadmurad <mahammad.muradd@gmail.com>
2024-01-14 14:36:35 +0400

    celery settings created

--- _celery/_celery/settings.py
+++ _celery/_celery/settings.py
@@ -28,6 +28,10 @@ DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+
 # Application definition
 
 INSTALLED_APPS = [
